4b544b86db690574c75e87df9a22e2193ad1a77d
Santiago Cepeda <santyagor@outlook.com>
2021-09-16 16:24:43 -0500

    ok control paginas error con vista personalizada

--- Vehicles.API/Startup.cs
+++ Vehicles.API/Startup.cs
@@ -34,6 +34,12 @@ namespace Vehicles.API
                 x.Password.RequireUppercase = false;
             }).AddEntityFrameworkStores<DataContext>();
 
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/NotAuthorized";
+                options.AccessDeniedPath = "/Account/NotAuthorized";
+            });
+
             services.AddDbContext<DataContext>(x =>
             {
                 x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
@@ -58,6 +64,7 @@ namespace Vehicles.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseStatusCodePagesWithReExecute("/error/{0}");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
